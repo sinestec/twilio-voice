86098a3464dc3a00b69c926bae3277cd9d45fed0
Evandro Couto <vandocouto@gmail.com>
2016-08-04 14:47:19 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# twilio-voice Sinestec
+
+- Este script tem como funçãdisparar um evento por voz para um determinado número de telefone.
+- Para executar o script é necessário criar um con no https://www.twilio.com
+
+Para executar o script, basta informar os dados:
+
+	*account
+	*token
+	*number-twilio
+	*number-you
+	*url-voice
+
+- Uitilize o Holdmusic para ler os arquivos em MP3 e reproduzi-los no formanto TwiML.
+	*Visite:
+	- https://www.twilio.com/labs/twimlets/holdmusic
+
+- Obs: Instale a biblioteca:
+<pre>
+# easy_install twilio
+</pre>
+ou
+<pre>
+# pip install twilio
+</pre>
---
+++ telefone.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+from twilio.rest import TwilioRestClient
+import sys
+
+def help_():
+    print 'Por favor, informe os campos abaixo:\n\n\
+"account" "token" "number-twilio" "number-you" "url-voice"\n\n\
+exemplo: python telefone.py "AC0630f7a85ddb" "0fb97b000" "55XXXXXXXXXX" "55XXXXXXXX" "http://twimlets.com/holdmusic?Bucket=XXX"'
+
+try:
+	recebe=sys.argv[1]
+	if (str(recebe) == "help"):
+		help_()
+	else:
+		account = sys.argv[1]
+		token = sys.argv[2]
+		de=sys.argv[3]
+		para=sys.argv[4]
+		msg=sys.argv[5]
+		client = TwilioRestClient(account, token)
+		try:
+        		call = client.calls.create(to=para,
+                       		from_=de,
+                       		url=msg)
+        		print (call.sid)
+		except:
+        		print ("Error-1")
+except:
+	print "'Digite help para obter ajuda'"
